9e1f0c492bcc4afc2d1767fc2a4a73afd81def1c
fatedpeace <fakepeaceful@proton.me>
2026-08-23 19:02:34 +1000

    updated animations, look  and feel

---
+++ .config/VSCodium/User/History/39b2755c/GR9f.lua
@@ -0,0 +1,56 @@
+-----------------------
+---- LOOK AND FEEL ----
+-----------------------
+
+
+-- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
+hl.config({
+    general = {
+        gaps_in  = 4,
+        gaps_out = 8,
+
+        border_size = 0,
+
+        col = {
+            active_border   = { colors = {"rgba(33ccffee)", "rgba(00ff99ee)"}, angle = 45 },
+            inactive_border = "rgba(595959aa)",
+        },
+
+        -- Set to true to enable resizing windows by clicking and dragging on borders and gaps
+        resize_on_border = false,
+
+        -- Please see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Tearing/ before you turn this on
+        allow_tearing = false,
+
+    },
+
+    decoration = {
+        rounding       = 20,
+        rounding_power = 2,
+
+        -- Change transparency of focused and unfocused windows
+        active_opacity   = 1.0,
+        inactive_opacity = 1.0,
+
+        shadow = {
+            enabled      = true,
+            range        = 15,
+            render_power = 4,
+            color        = 0xee0a0a0a,
+        },
+
+        blur = {
+            enabled   = true,
+            xray =  false,
+            special = false,
+            ignore_opacity = true, 
+            new_optimizations = true,
+            popups = true,
+            input_methods = true, 
+            size      = 8,
+            passes    = 2,
+            vibrancy  = 0.1696,
+        },
+    },
+})
+
